f2ca3621d42670c028e7e584add09c3c0133d46b
akherlan <herlandi.27@gmail.com>
2022-08-10 19:50:21 +0700

    Process 3

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ payload = payload.split('\n')
 payloadfile.close()
 
 
-# Main process (process #2)
-for req in payload[5000:10000]:
+# Main process (process #3)
+for req in payload[10000:15000]:
     # Give some informations about process
     pg = req.split('&')[0].replace('page=', '')
     lv = req.split('&')[4].replace('bentuk_pendidikan=', '').replace('+',' ')
